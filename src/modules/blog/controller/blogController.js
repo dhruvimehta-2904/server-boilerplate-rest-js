ecfa2a0fbd5f58b05db1220de78113781a89d57e
dhruvi <mehtadhruvi2907@gmail.com>
2026-02-11 09:22:26 +0530

    aggregate pipeline implement in blog

--- src/modules/blog/controller/blogController.js
+++ src/modules/blog/controller/blogController.js
@@ -1,5 +1,8 @@
-const Blog = require("../../../schema/blog.model")
+const Blog = require("../../../schema/blog.model");
+const asyncHandler = require("../../../utils/async-handler");
 const ApiResponse = require("../../../utils/response");
+const mongoose = require("mongoose");
+
 
 module.exports.createBlog = async(req,res)=>{
     try {
@@ -20,33 +23,65 @@ console.log(blog);
     }
 }
 
-module.exports.get_all = async(req,res)=>{
-    try {
-        const userId  = req.user._id
-    // const {title, content, tags, category, isPublished, views, status} = req.body;
+// module.exports.get_all = async(req,res)=>{
+//     try {
+//         const userId  = req.user._id
+//     // const {title, content, tags, category, isPublished, views, status} = req.body;
 
-    const blog = await Blog.find({author:userId})
-        res.status(200).json({sucess:true, message:"all blogs", blog:blog})
+//     const blog = await Blog.find({author:userId})
+//         res.status(200).json({sucess:true, message:"all blogs", blog:blog})
         
-    } catch (error) {
-        res.status(400).json({sucess:false})
-    }
-}
+//     } catch (error) {
+//         res.status(400).json({sucess:false})
+//     }
+// }
+
+module.exports.get_all = asyncHandler(async(req,res)=>{
+    const userId = req.user._id
+    console.log(userId);
+    
+    const user = await Blog.aggregate([
+        {
+            $match:{
+                author:userId
+            }
+        }
+    ])
+     res.status(200).json({sucess:true, message:"all blogs", user})
+})
 
 module.exports.get_all_by_id = async(req,res)=>{
     try {
-       
-
         // const id = req.user.id
-        const authorId = req.params.id
+        const authorId = new mongoose.Types.ObjectId(req.params.id) 
+console.log(authorId);
 
-        const data = req.body
-        const blog = await Blog.find({author:authorId})
-        res.status(200).json({success:true, message:"blogs", blog:blog})
+        // const data = req.body
+        // const blog = await Blog.find({author:authorId})
+
+const blog = await Blog.aggregate([
+    {
+        $match:{
+            author: authorId
+        }
+    }
+])
+console.log(blog);
+
+
+        res.status(200).json({
+            blog,
+            message: "blogs",
+            statusCode: 200
+        })
 
     } catch (error) {
-        res.status(400).json({sucess:false})
-    }
+    console.error("ERROR:", error); 
+    res.status(500).json({
+        success: false,
+        message: error.message
+    });
+}
 }
 
 
